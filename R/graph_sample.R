70008aac3f4517f7b92847f43f06fac08d43a02d
Adeelaiide <adelaide.perruchon@imev-mer.fr>
2025-06-19 11:43:43 +0200

    Update graph_sample.R

--- R/graph_sample.R
+++ R/graph_sample.R
@@ -174,13 +174,7 @@ p7<-ggplot(plot_data) +
   labs(x = "Log Biovolume +1 (mm³⋅m⁻³)", y = NULL) +
   ggtitle("Trophic pyramid") + 
   theme_classic() +
-  theme(plot.title = element_text(hjust = 0.5, size = 10,face = "bold"), 
-        legend.text = element_text(size = 0.5),
-        axis.title.x = element_text(size = 8),
-        axis.line.x = element_line(colour = "black"),
-        axis.text.y = element_text(size = 8, hjust = 1), 
-        axis.ticks.y = element_line(colour = "black"),   
-        axis.line.y = element_line(colour = "black"))
+  theme(plot.title = element_text(hjust = 0.5, size = 10,face = "bold"), legend.text = element_text(size = 0.5))
 
   # Map
     ##Extract Lat/Lon, load world map and convert xy points as sf objects
