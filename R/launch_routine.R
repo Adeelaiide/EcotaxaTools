6ac5c2c31df997bce62ef0758d80f9f5589db446
Adeelaiide <adelaide.perruchon@imev-mer.fr>
2025-06-23 09:58:21 +0200

    Update launch_routine.R

--- R/launch_routine.R
+++ R/launch_routine.R
@@ -44,7 +44,6 @@ if (!is.null(mainpath) && mainpath != "") {
 
   # Set common color palette
   plankton_groups_colors <- c("#709699", #cyanobacteria
-               "#F2F2F2", #detritus
                "#FAFABA", #other
                "#C9C4FF", #ciliophora
                "#B5D493", #dinoflagellata
@@ -59,13 +58,12 @@ if (!is.null(mainpath) && mainpath != "") {
                "#C68181", #mollusca
                "#668F3B", #coccolithophyceae
                "#FFD3CF", #other_unidentified
-               "#0073BD" #plastics
-               )
+              )
   
-  names(plankton_groups_colors)<- c("cyanobacteria","detritus","other","ciliophora","dinoflagellata",
+  names(plankton_groups_colors)<- c("cyanobacteria","other","ciliophora","dinoflagellata",
                      "rhizaria","bacillariophyta","dictyochophyceae","crustacea",
                      "copepoda","chaetognatha","tunicata","cnidaria","mollusca",
-                     "coccolithophyceae","other_unidentified","plastics")
+                     "coccolithophyceae","other_unidentified")
   plankton_groups_colScale <- scale_colour_manual(name = "taxonomic group",values = plankton_groups_colors)
   plankton_groups_colFill <- scale_fill_manual(name = "taxonomic group",values = plankton_groups_colors)
 
