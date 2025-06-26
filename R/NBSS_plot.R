883ba033ffcadf5ab11ea7a7781cabc8fc438227
Adeelaiide <adelaide.perruchon@imev-mer.fr>
2025-06-26 16:35:08 +0200

    Update NBSS_plot.R

--- R/NBSS_plot.R
+++ R/NBSS_plot.R
@@ -20,7 +20,31 @@ NBSS.plot <- function(x, taxon, bv.type, samples="all"){
 
   # Define colors according to common taxonomy vector
   N <- length(taxon)
-  myColors <- colorRampPalette(brewer.pal(8, "Set2"))(N)
+ plankton_groups_colors <- c("#709699", #cyanobacteria
+               "#F2F2F2", #detritus
+               "#FAFABA", #other
+               "#C9C4FF", #ciliophora
+               "#B5D493", #dinoflagellata
+               "#FAD4EB", #rhizaria
+               "#1A9C75", #bacillariophyta
+               "#E3E699", #dictyochophyceae
+               "#EDB022", #crustacea
+               "#E88F6B", #copepoda
+               "#B0D6E0", #chaetognatha
+               "#3B638A", #tunicata
+               "#6999C7", #cnidaria
+               "#C68181", #mollusca
+               "#668F3B", #coccolithophyceae
+               "#FFD3CF", #other_unidentified
+               "#0073BD" #plastics
+               )
+
+  names(plankton_groups_colors)<- c("cyanobacteria","detritus","other","ciliophora","dinoflagellata",
+                     "rhizaria","bacillariophyta","dictyochophyceae","crustacea",
+                     "copepoda","chaetognatha","tunicata","cnidaria","mollusca",
+                     "coccolithophyceae","other_unidentified","plastics")
+  plankton_groups_colScale <- scale_colour_manual(name = "taxonomic group",values = plankton_groups_colors)
+  plankton_groups_colFill <- scale_fill_manual(name = "taxonomic group",values = plankton_groups_colors)
 
   if(samples=="all") samples <- unique(x$sample_num)
 
@@ -37,7 +61,7 @@ NBSS.plot <- function(x, taxon, bv.type, samples="all"){
     g <- x %>% filter(type==bv.type, object_annotation_category %in% taxon, sample_id %in% samples) %>%
       ggplot(aes(x=max, y=BV/norm, color=taxon)) +
       geom_line() +
-      scale_color_manual(values = myColors) +
+      plankton_groups_colFill +
       geom_point() +
       scale_x_log10("Size (mm)", limits=c(mini,maxi)) +
       scale_y_log10("NBSS (mm\u00b3.mm\u207B\u00b3.m\u207B\u00b3)", labels=trans_format('log10',math_format(10^.x))) +
