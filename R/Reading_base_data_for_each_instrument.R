d862c0fd21a03689e80fc26de1e01ddeb85e95d3
Adeelaiide <adelaide.perruchon@imev-mer.fr>
2025-08-04 14:36:36 +0200

    Update Reading_base_data_for_each_instrument.R

--- R/Reading_base_data_for_each_instrument.R
+++ R/Reading_base_data_for_each_instrument.R
@@ -38,6 +38,8 @@ transform_planktoscope_data <- function(df) {
            percentValidated,
            number_object,
            acq_nb_frame,
+           acq_minimum_mesh,
+           acq_maximum_mesh,
            sample_total_volume,
            sample_concentrated_sample_volume,
            acq_celltype,
@@ -79,6 +81,8 @@ transform_flowcam_data <- function(df) {
            number_object,
            acq_raw_image_total,
            acq_celltype,
+           acq_min_esd,
+           acq_max_esd
            sample_initial_col_vol_m3,
            sample_conc_vol_ml,
            acq_fluid_volume_imaged,
@@ -88,3 +92,47 @@ transform_flowcam_data <- function(df) {
     group_by(sample_id) %>% mutate(ghost_id=1:n()) %>% ungroup() 
 }
 
+# Function for Zooscan specific data transformation
+transform_zooscan_data <- function(df) {
+  df %>%
+    group_by(object_id, acq_id) %>% 
+    mutate(ghost_id = 1:n()) %>%
+    ungroup() %>%
+    mutate(unique_id = paste(acq_id, sample_scan_operator, ghost_id, 
+                             object_date, object_time,
+                             object_lat, object_lon, sep = "_")) %>%
+   group_by(unique_id) %>%
+    mutate(number_object = n(),
+           percentValidated = sum(object_annotation_status == "validated", na.rm = TRUE) / n() * 100) %>%
+    ungroup() %>%
+   mutate(major = object_major * pixelsize,
+      minor = object_minor * pixelsize,
+      area_exc = object_area_exc * (pixelsize^2),
+      area = object_area * (pixelsize^2),
+      perimferet = object_feret * pixelsize,
+      ESD = 2 * (((object_area * (pixelsize^2)) / pi)^0.5),
+      conver = unique(acq_sub_part) / unique(sample_tot_vol)) %>%
+  select(sample_id,
+      sample_scan_operator,
+      sample_barcode,
+      sample_tot_vol,
+      object_date,
+      object_time,
+      object_lat,
+      object_lon,
+      acq_id,
+      acq_min_mesh,
+      acq_max_mesh,
+      acq_sub_part,
+      process_img_resolution,
+      percentValidated,
+      major,
+      minor,
+      area_exc,
+      area,
+      perimferet,
+      ESD,
+      conver) %>%
+   distinct() %>%
+    group_by(sample_id) %>% mutate(ghost_id=1:n()) %>% ungroup() 
+}
