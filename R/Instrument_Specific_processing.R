2281d710a8eb57457115f213a8b9ef6ce5ff43a9
Adeelaiide <adelaide.perruchon@imev-mer.fr>
2025-08-11 10:24:45 +0200

    Update Instrument_Specific_processing.R

--- R/Instrument_Specific_processing.R
+++ R/Instrument_Specific_processing.R
@@ -150,7 +150,7 @@ process_flowcam_data <- function(data, metadata) {
 process_zooscan_data <- function(data, metadata) {
   # ZooScan-specific unit conversions
   data <- mutate(metadata,
-                pixelsize = unique(process_pixel) #,
+                pixelsize = unique(process_particle_pixel_size_mm) #,
                 #perimferet = object_feret * pixelsize
                 )
   
@@ -189,6 +189,7 @@ process_zooscan_data <- function(data, metadata) {
   
   #return(data)
 #}
+
 
 
 
