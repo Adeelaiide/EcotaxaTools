e684394d51526589f44ef6ab54a3e689705f5711
HZaccomer <zaccomer.hugo966@gmail.com>
2026-01-28 15:07:45 +0100

    Update launch_routine.R
    Delete color palette as it is already done in the graph_project / graph_sample code

--- R/launch_routine.R
+++ R/launch_routine.R
@@ -48,34 +48,6 @@ if (!is.null(mainpath) && mainpath != "") {
   # Check metadata
   processed_metadata <- check_metadata(path, output, instru)
 
-  # Set common color palette
-  plankton_groups_colors <- c("#709699", #cyanobacteria
-               "#F2F2F2", #detritus
-               "#FAFABA", #other
-               "#C9C4FF", #ciliophora
-               "#B5D493", #dinoflagellata
-               "#FAD4EB", #rhizaria
-               "#1A9C75", #bacillariophyta
-               "#E3E699", #dictyochophyceae
-               "#EDB022", #crustacea
-               "#E88F6B", #copepoda
-               "#B0D6E0", #chaetognatha
-               "#3B638A", #tunicata
-               "#6999C7", #cnidaria
-               "#C68181", #mollusca
-               "#668F3B", #coccolithophyceae
-               "#FFD3CF", #other_unidentified
-               "#0073BD" #plastics
-               )
-
-  names(plankton_groups_colors)<- c("cyanobacteria","detritus","other","ciliophora","dinoflagellata",
-                     "rhizaria","bacillariophyta","dictyochophyceae","crustacea",
-                     "copepoda","chaetognatha","tunicata","cnidaria","mollusca",
-                     "coccolithophyceae","other_unidentified","plastics")
-  plankton_groups_colScale <- scale_colour_manual(name = "Taxonomic group",values = plankton_groups_colors)
-  plankton_groups_colFill <- scale_fill_manual(name = "Taxonomic group",values = plankton_groups_colors)
-
-
   # Compute biovolumes and BSS summary (warning : not normalized by size class)
   yesno <- dlg_message("IMPORTANT: Do you want to use the edited metadata ? If not you can select the original metadata or another metadata table", type="yesno")$res
 
